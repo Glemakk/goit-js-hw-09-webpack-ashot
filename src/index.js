29c89eb5c8498255a369b96d6c3f9b17e562a450
Glemakk <kovgar.brand@gmail.com>
2021-04-25 12:39:27 +0200

    добавлен counter

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
-import "./styles.scss";
-import sumNumbers from "./utils.js";
+import "./styles/styles.scss";
+
+//import template from ".template/main.hbs";
+import sumNumbers from "./js/utils.js";
+
+const num1Input = document.querySelector('#num1');
+const num2Input = document.querySelector('#num2');
+const btn = document.querySelector('#sumBtn');
+
+const result = document.getElementById('result');
+
+btn.addEventListener('click', (e) => {
+    const num1 = Number(num1Input.value);
+    const num2 = Number(num2Input.value);
+
+    result.innerHTML = sumNumbers(num1, num2)
+})
 
 console.log(sumNumbers(2, 3));
